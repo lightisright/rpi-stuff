104075d5df214ca001ef4af80b61b2ac01574ee6
Unknown <x.correyeur@free.fr>
2018-12-20 00:32:01 +0100

    add python power skeleton

---
+++ teleinfo-edf/python/power.py
@@ -0,0 +1,66 @@
+
+
+class TeleinfoEdf(ABC.abc)
+
+    def __init__(self):
+        pass
+
+    def getPower (self, int: numberOfDays):
+        """
+        //
+        //  recupere les donnees de puissance des $nb_days derniers jours et les met en forme pour les affichers sur le graphique
+        //
+        
+        global $sqlite;
+        $months    = array('01' => 'janv', '02' => 'fev', '03' => 'mars', '04' => 'avril', '05' => 'mai', '06' => 'juin', '07' => 'juil', '08' => 'aout', '09' => 'sept', '10' => 'oct', '11' => 'nov', '12' => 'dec');
+        $now  = time();
+        $past = strtotime("-$nb_days day", $now);
+
+        $db = new SQLite3($sqlite);
+        $results = $db->query("SELECT * FROM puissance WHERE timestamp > $past ORDER BY timestamp ASC;");
+
+        $sums = array();
+        $days = array();
+        $datas = array();
+
+        while($row = $results->fetchArray(SQLITE3_ASSOC)){
+        $year   = date("Y", $row['timestamp']);
+        $month  = date("n", $row['timestamp']-1);
+        $day    = date("j", $row['timestamp']);
+        $hour   = date("G", $row['timestamp']);
+        $minute = date("i", $row['timestamp']);
+        $second = date("s", $row['timestamp']);
+        $datas[] = "[{v:new Date($year, $month, $day, $hour, $minute, $second), f:'".date("j", $row['timestamp'])." ".$months[date("m", $row['timestamp'])]." ".date("H\hi", $row['timestamp'])."'}, {v:".$row['va'].", f:'".$row['va']." V.A'}, {v:".$row['watt'].", f:'".$row['watt']." kW'}]";
+
+        }
+
+        return implode(', ', $datas);
+        }
+        """
+        
+        def getConso (self, int: numberOfDays) {
+        """
+
+         //
+        //  recupere les donnees de consommation des $nb_days derniers jours et les met en forme pour les affichers sur le graphique
+        //
+        function getDatasConso ($nb_days) {
+            global $sqlite;
+            $months    = array('01' => 'janv', '02' => 'fev', '03' => 'mars', '04' => 'avril', '05' => 'mai', '06' => 'juin', '07' => 'juil', '08' => 'aout', '09' => 'sept', '10' => 'oct', '11' => 'nov', '12' => 'dec');
+            $now  = time();
+            $past = strtotime("-$nb_days day", $now);
+
+            $db = new SQLite3($sqlite);
+            $results = $db->query("SELECT * FROM conso WHERE timestamp > $past ORDER BY timestamp ASC;");
+
+            $datas = array();
+
+            while($row = $results->fetchArray(SQLITE3_ASSOC)){
+            $day    = date("j", $row['timestamp'])." ".$months[date("m", $row['timestamp'])];
+            $datas[] = "['".$day."', {v:".$row['daily_hp'].", f:'".$row['daily_hp']." kWh'}, {v:".$row['daily_hc'].", f:'".$row['daily_hc']." kWh'}]";
+            }
+
+            return implode(', ', $datas);
+        }
+
+        """
