d3565e4f80ec8a9141715ddec7e15b946c7c3aac
Unknown <x.correyeur@free.fr>
2019-03-07 00:30:33 +0100

    get power from db within json format

--- teleinfo-edf/python/power.py
+++ teleinfo-edf/python/power.py
@@ -5,64 +5,47 @@ import datetime
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
+        if col[0] == 'timestamp':
+            d['date'] = datetime.datetime.fromtimestamp(row[idx]).isoformat()
+        else:
+            d[col[0]] = row[idx]
     return d
 
-class TeleinfoEdf(ABC.abc)
+class TeleinfoEdf():
 
     def __init__(self):
         pass
 
-    def getPower (self, int: numberOfDays):
-        """
-        //
-        //  recupere les donnees de puissance des $nb_days derniers jours et les met en forme pour les affichers sur le graphique
-        //
+    def debug(self,msg: str):
+        print(msg)
+
+    def getPower (self, numberOfDays:int):
         
-        global $sqlite;
-        $months    = array('01' => 'janv', '02' => 'fev', '03' => 'mars', '04' => 'avril', '05' => 'mai', '06' => 'juin', '07' => 'juil', '08' => 'aout', '09' => 'sept', '10' => 'oct', '11' => 'nov', '12' => 'dec');
-        $now  = time();
-        """
         db = sqlite3.connect("teleinfo-edf.sqlite")
-        delta = datetime.timedelta(days=(-1*numberOfDays))
-        dt = datetime.datetime(delta.year,delta.month,delta.day).timestamp()
-        nw = datetime.datetime.now().timestamp()
-        d = datetime.timedelta(days=(1*7)).total_seconds()
-        min = nw-d
 
-        db.row_factory = dict_factory
-        cursor = db.cursor()
-        db.execute("SELECT * FROM puissance WHERE timestamp > "+str(int(min))+" and timestamp < "+str(int(nw))+" ORDER BY timestamp ASC;")
-        results = cursor.fetchall()
-        print results
-        db.close()
- 
-        """
-        $past = strtotime("-$nb_days day", $now);
+        d2=datetime.datetime.now()
+        t2=d2.timestamp()
 
-        $db = new SQLite3($sqlite);
-        $results = $db->query("SELECT * FROM puissance WHERE timestamp > $past ORDER BY timestamp ASC;");
-
-        $sums = array();
-        $days = array();
-        $datas = array();
-
-        while($row = $results->fetchArray(SQLITE3_ASSOC)){
-        $year   = date("Y", $row['timestamp']);
-        $month  = date("n", $row['timestamp']-1);
-        $day    = date("j", $row['timestamp']);
-        $hour   = date("G", $row['timestamp']);
-        $minute = date("i", $row['timestamp']);
-        $second = date("s", $row['timestamp']);
-        $datas[] = "[{v:new Date($year, $month, $day, $hour, $minute, $second), f:'".date("j", $row['timestamp'])." ".$months[date("m", $row['timestamp'])]." ".date("H\hi", $row['timestamp'])."'}, {v:".$row['va'].", f:'".$row['va']." V.A'}, {v:".$row['watt'].", f:'".$row['watt']." kW'}]";
-
-        }
-
-        return implode(', ', $datas);
-        }
-        """
+        delta = datetime.timedelta(days=(-1*numberOfDays)).total_seconds()
+        t1=t2+delta
+        d1=datetime.datetime.fromtimestamp(t1)
         
-        def getConso (self, int: numberOfDays) {
+        self.debug(f"d1: {d1.isoformat()} - d2: {d2.isoformat()}")
+        self.debug(f"t1: {t1} - t2: {t2}")
+
+        req = f"SELECT * FROM puissance WHERE timestamp > {str(int(t1))} and timestamp < {str(int(t2))} ORDER BY timestamp ASC;"
+        
+        cursor = db.cursor()
+        cursor.row_factory = dict_factory
+        cursor.execute(req)
+        
+        result = cursor.fetchall()
+        self.debug(result)
+        db.close()
+        return(result)
+        
+    def getConso (self, numberOfDays:int):
+        return False
         """
 
          //
